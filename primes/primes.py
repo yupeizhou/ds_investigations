558f6071b42d91365e765d716790229b03b79af9
Aaron Fraenkel <aaron.fraenkel@gmail.com>
2019-07-02 11:21:44 -0700

    primes: first commit

---
+++ primes/primes.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+
+def primesfrom2to(n):
+    """ Input n>=6, Returns a array of primes, 2 <= p < n """
+    sieve = np.ones(n//3 + (n%6==2), dtype=np.bool)
+    for i in range(1,int(n**0.5)//3+1):
+        if sieve[i]:
+            k=3*i+1|1
+            sieve[       k*k//3     ::2*k] = False
+            sieve[k*(k-2*(i&1)+4)//3::2*k] = False
+    return np.r_[2,3,((3*np.nonzero(sieve)[0][1:]+1)|1)]
